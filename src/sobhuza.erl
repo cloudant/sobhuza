f3d3afac9bb737198468b8d622e7863c3bfed7fe
Robert Newson <rnewson@apache.org>
2015-05-16 12:20:28 +0100

    Constant time election

--- src/sobhuza.erl
+++ src/sobhuza.erl
@@ -7,10 +7,12 @@
 -record(state, {
     delta,
     last_alert,
+    last_ping,
     last_restart,
     leader,
     nodes,
     ok_count,
+    pongs,
     round,
     tick,
     timer
@@ -29,9 +31,10 @@ init({Delta, Nodes}) ->
     State = #state{
         delta = Delta,
 	last_alert = 0,
-	last_restart = 0,
+        last_restart = 0,
 	nodes = lists:sort(Nodes),
         ok_count = 0,
+	pongs = [],
 	tick = 0,
 	timer = timer:send_interval(Delta, tick)
     },
@@ -82,15 +85,40 @@ handle_cast({alert, Round}, State) when Round > State#state.round ->
     error_logger:info_msg("Leader demoted by alert from round ~B~n", [Round]),
     {noreply, State#state{last_alert = State#state.tick, leader = undefined}};
 
+handle_cast({ping, Round, From}, State) ->
+    error_logger:info_msg("Received ping from ~p in round ~B~n", [From, Round]),
+    send({pong, Round, node()}, From),
+    {noreply, State};
+
+handle_cast({pong, Round, From}, State0) when Round == State0#state.round ->
+    error_logger:info_msg("Received pong from ~p in current round ~B~n", [From, Round]),
+    State1 = State0#state{pongs = [From | State0#state.pongs]},
+    {noreply, State1};
+
+handle_cast({pong, Round, From}, State) ->
+    error_logger:info_msg("Received pong from ~p in round ~B~n", [From, Round]),
+    {noreply, State};
+
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 
 handle_info(tick, State) ->
-    case (State#state.tick - State#state.last_restart) > 2 of
-	true ->
-	    {noreply, start_round(State#state.round + 1, State)};
-	false ->
+    Pinging = State#state.last_ping /= undefined,
+    PingComplete = Pinging andalso (State#state.tick - State#state.last_ping) == 2,
+    Timeout = (State#state.tick - State#state.last_restart) > 2,
+    case {Pinging, PingComplete, Timeout} of
+	{true, false, _} ->
+	    {noreply, tick(State)};
+	{true, true, _} ->
+	    Round = next_round(State),
+	    error_logger:info_msg("Jumping to responsive round ~B~n", [Round]),
+	    {noreply, tick(start_round(Round, State#state{last_ping = undefined}))};
+	{false, false, true} ->
+	    send_all({alert, State#state.round + 1}, State),
+	    send_all({ping, State#state.round, node()}, State),
+	    {noreply, tick(State#state{last_ping = State#state.tick})};
+	{false, false, false} ->
 	    case candidate(State) of
 		Node when Node == node() ->
 		    send_all({ok, State#state.round, node()}, State);
@@ -112,7 +140,17 @@ code_change(_OldVsn, State, _Extra) ->
 %% private functions
 
 candidate(#state{round = Round, nodes = Nodes}) ->
+    candidate(Round, Nodes).
+
+candidate(Round, Nodes) ->
     lists:nth(Round rem length(Nodes) + 1, Nodes).
+
+
+next_round(#state{round = Round, nodes = Nodes, pongs = Pongs}) ->
+    L0 = lists:seq(Round + 1, Round + 1 + length(Nodes)),
+    L1 = [{S, candidate(S, Nodes)} || S <- L0],
+    L2 = [S || {S, Candidate} <- L1, lists:member(Candidate, Pongs)],
+    hd(L2).
 
 
 tick(State) ->
@@ -134,9 +172,11 @@ send_all(Msg, State) ->
 start_round(Round, State0) ->
     error_logger:info_msg("Starting round ~B~n", [Round]),
     State = restart_timer(State0#state{
+        last_ping = undefined,
         leader = undefined,
         ok_count = 0,
-	round = Round
+	round = Round,
+        pongs = []
     }),
     send_all({alert, Round}, State),
     case candidate(State) of
